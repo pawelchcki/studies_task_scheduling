5057b24a8353bfe2411f58c6170dd368e3a0fdd5
Pawel Chojnacki <pawel@chojnacki.ws>
2015-01-07 01:18:36 +0100

    multi processing

--- src/ws/chojnacki/Main.java
+++ src/ws/chojnacki/Main.java
@@ -1,8 +1,11 @@
 package ws.chojnacki;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 public class Main {
     public static final int NOT_SET = -1;
@@ -37,9 +40,34 @@ public class Main {
         System.err.println("");
     }
 
-//    int multiProcess(List<Integer> procs, int[] originalCpus){
-//        List<Integer> procs = new Arra
-//    }
+    int multiProcess(List<Integer> procs, int[] originalCpus){
+        List<Integer> rvList = Collections.synchronizedList(new ArrayList<>());
+        Function<Integer, Runnable> r = (Integer numProc) -> () -> {
+            List<Integer> nuProc = new LinkedList<>(procs);
+            Collections.copy(nuProc, procs);
+            int[] nuCpus = originalCpus.clone();
+            int procVal = nuProc.remove(numProc.intValue());
+            int mTime = NOT_SET;
+            for (int i=0; i<originalCpus.length; i++){
+                mTime = process(nuProc, nuCpus, i, procVal, mTime);
+            }
+            rvList.add(mTime);
+        };
+        List<Thread> threads = new LinkedList<>();
+        for(int i=0; i< procs.size(); i++){
+            Thread th = new Thread(r.apply(i));
+            threads.add(th);
+            th.start();
+        }
+        for (Thread th: threads) {
+            try {
+                th.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return Collections.min(rvList);
+    }
 
     int process(List<Integer> procs, int[] originalCpus, int iCpu, int procVal, int mTime) {
         int rv;
@@ -67,12 +95,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] procs = {5, 5, 4, 4, 3, 3, 3,1};
-//        |5 4 3 3
-//        |5 4 3
-//        |5 4 3
+        int[] procs = {5, 5, 4, 4, 3, 3, 3, 1, 2};
         Main m = new Main();
-        int x = m.process(m.initProcs(procs), m.initCpus(3), 0, 0, NOT_SET);
+//        int x = m.process(m.initProcs(procs), m.initCpus(3), 0, 0, NOT_SET);
+        int x = m.multiProcess(m.initProcs(procs), m.initCpus(3));
+
         System.err.println(x);
     }
 }
